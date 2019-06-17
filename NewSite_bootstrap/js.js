3f3d54d76e0ec8d9f77d8815892dce841e2cecc1
Paladin00 <harshperson@gmail.com>
2019-06-17 12:39:23 +0300

    123

---
+++ NewSite_bootstrap/js.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+    document.querySelector(".tabs-header").addEventListener('click', nav-item);
+
+    function ClickTab(event) {
+        var arrTabs = document.getElementsByClassName('tab');
+        for (var i = 0; i < arrTabs.length; i++) {
+            arrTabs[i].classList.remove('tabActive');
+        }
+
+        if (event.target.className == "tab") {
+            var dataTab = event.target.getAttribute('data-tab');
+
+            var arrTabBody = document.getElementsByClassName('tab-body');
+            for (var i = 0; i < arrTabBody.length; i++) {
+                if (dataTab == i) {
+                    arrTabBody[i].style.display = 'block';
+                    event.target.classList.add('tabActive');
+                } else {
+                    arrTabBody[i].style.display = 'none';
+
+                }
+            }
+        }
+    }
+}
